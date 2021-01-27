2e486e62cb153dde60efda680b95e1015d9bc14c
“Raqib <raqib850@gmail.com>
2021-01-27 23:55:23 +0530

    some fixes

--- src/features/home/App.js
+++ src/features/home/App.js
@@ -9,38 +9,30 @@ export default function App({ children }) {
 </div>
   );
 }*/
-import React from "react";
-import { Switch,Route, Redirect } from "react-router-dom";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Contact from "./Contact";
-import Home from "./Home";
-import Services from "./Services";
-import About from "./About";
-import Navbar from "./Navbar"
+import Contact from './Contact';
+import Home from './Home';
+import Services from './Services';
+import About from './About';
+import Navbar from './Navbar';
 
-
- const App = () => 
-{
-  return(
+const App = () => {
+  return (
     <>
-    <Navbar/>
-    <Switch >
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={About} />
-    <Route exact path="/ service" component={Services} />
-    <Route exact path="/contact" component={Contact} />
-    
-  <Redirect to ="/"/>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/ service" component={Services} />
+        <Route exact path="/contact" component={Contact} />
 
-  
+        <Redirect to="/" />
 
-    <Home />
-    </Switch>
-    
-</>
-  )
-
-}
+        <Home />
+      </Switch>
+    </>
+  );
+};
 export default App;
